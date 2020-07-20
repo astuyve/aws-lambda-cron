62e590745702fa94c38a5a6f1ec87ffa9e7195a1
astuyve <astuyve@gmail.com>
2020-07-19 20:18:05 -0500

    Remove working

--- src/serverless.js
+++ src/serverless.js
@@ -11,9 +11,12 @@ class LambdaCron extends Component {
       throw new Error(msg);
     }
 
+    const region = inputs.region || "us-east-1";
+
+    this.state.region = region;
     aws.config.update({
       credentials: this.credentials.aws,
-      region: inputs.region,
+      region,
     });
 
     const roleParams = {
@@ -37,17 +40,19 @@ class LambdaCron extends Component {
       ],
     };
     const { roleArn } = await aws.utils.deployRole(roleParams);
+    this.state.roleName = roleParams.roleName;
 
     const lambdaParams = {
       lambdaName: `${inputs.name}-lambda`, // required
       roleArn,
       lambdaSrc: await fs.promises.readFile(inputs.src),
-      memory: 512,
+      memory: inputs.memory || 512,
     };
 
     const { lambdaArn, lambdaSize, lambdaSha } = await aws.utils.deployLambda(
       lambdaParams
     );
+    this.state.lambdaName = lambdaParams.lambdaName;
 
     const putRuleParams = {
       Name: `${inputs.name}-rule`,
@@ -57,6 +62,7 @@ class LambdaCron extends Component {
 
     const cwEvents = new aws.CloudWatchEvents();
     const { RuleArn } = await cwEvents.putRule(putRuleParams).promise();
+    this.state.cloudWatchRule = putRuleParams.Name;
 
     const lambda = new aws.Lambda();
     const lambdaPermissions = {
@@ -67,7 +73,11 @@ class LambdaCron extends Component {
       SourceArn: RuleArn,
     };
 
-    await lambda.addPermission(lambdaPermissions).promise();
+    try {
+      await lambda.addPermission(lambdaPermissions).promise();
+    } catch (error) {
+      console.log("permission already added to lambda, continuing");
+    }
 
     const targetParams = {
       Rule: putRuleParams.Name,
@@ -79,6 +89,25 @@ class LambdaCron extends Component {
       ],
     };
     const response = await cwEvents.putTargets(targetParams).promise();
+    this.state.targetId = targetParams.Targets.Id;
+  }
+  async remove() {
+    if (Object.keys(this.credentials.aws).length === 0) {
+      const msg = `Credentials not found. Make sure you have a .env file in the cwd. - Docs: https://git.io/JvArp`;
+      throw new Error(msg);
+    }
+
+    aws.config.update({
+      credentials: this.credentials.aws,
+      region: this.state.region || "us-east-1",
+    });
+
+    await aws.utils.removeRole({ roleName: this.state.roleName });
+    await aws.utils.removeLambda({ lambdaName: this.state.lambdaName });
+    const cwEvents = new aws.CloudWatchEvents();
+    await cwEvents.deleteRule(this.state.cloudWatchRule);
+    // Clear state, we did it team
+    this.state = {};
   }
 }
 
